d43e9e1af4f87df5a8415dcafc519a8746ac9003
Siddharth <siddharthalrr@gmail.com>
2026-07-17 13:57:59 +0530

    siddharth

---
+++ siddharth/src/main/java/com/siddharthalrr/siddharth/SiddharthApplication.java
@@ -0,0 +1,13 @@
+package com.siddharthalrr.siddharth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiddharthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiddharthApplication.class, args);
+	}
+
+}
---
+++ siddharth/src/test/java/com/siddharthalrr/siddharth/SiddharthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharthalrr.siddharth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiddharthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
