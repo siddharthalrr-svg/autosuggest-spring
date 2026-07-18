d097c2cacc5af61ecca6d670b4658652c63f6d95
Siddharth <siddharthalrr@gmail.com>
2026-07-18 11:52:22 +0530

    toggle user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,1 +1,37 @@
-alert("get a life")
+// alert("get a life")
+
+console. log("alright")
+var users = [
+    {
+        "name" : "Charlie Kirk",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Summer Roberts",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
+
+
